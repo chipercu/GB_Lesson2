89ebb19c8c346b9f0bd3796901bee44ac1f2245a
Caris <chipercualexandru@gmail.com>
2022-04-05 12:35:16 +0300

    refactored

--- src/main/java/lesson5/ThreadHomework.java
+++ src/main/java/lesson5/ThreadHomework.java
@@ -16,14 +16,16 @@ public class ThreadHomework {
             arr[i] = 1.0f;
         }
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+        dataExec(arr);
         System.out.println("One thread time: " + (System.currentTimeMillis() -
                 startTime) + " ms.");
     }
 
-
+    public static void dataExec(float[] data){
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (float) (data[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
     public static void secondMethod(int size) {
         float[] arr = new float[size];
         Arrays.fill(arr, 1.0f);
@@ -36,19 +38,10 @@ public class ThreadHomework {
         System.arraycopy(arr, 0, leftHalf, 0, size/2);
         System.arraycopy(arr, size/2, rightHalf, 0, size/2);
 
-        final Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < leftHalf.length; i++) {
-                leftHalf[i] = (float) (leftHalf[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        });
+        final Thread thread1 = new Thread(() -> dataExec(leftHalf));
+        final Thread thread2 = new Thread(() -> dataExec(rightHalf));
 
-
-
-        final Thread thread2 = new Thread(() -> {
-            for (int i = 0;i < rightHalf.length; i++) {
-                rightHalf[i] = (float) (rightHalf[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        });
+        // Запускает два потока и параллельно просчитываем каждый малый массив
         thread1.start();
         thread2.start();
 
@@ -58,17 +51,10 @@ public class ThreadHomework {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        // Склеиваем малые массивы обратно в один большой
         System.arraycopy(leftHalf, 0, arr, 0, size/2);
         System.arraycopy(rightHalf, 0, arr, size/2, size/2);
 
-
-
-
-// Запускает два потока и параллельно просчитываем каждый малый массив
-// ...
-// Склеиваем малые массивы обратно в один большой
-// ...
         System.out.println("Two thread time: " + (System.currentTimeMillis() -
                 startTime) + " ms.");
     }
